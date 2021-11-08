179fa7fd1a22fdeed3332c91c02eec8dd9f87b6a
Talwind1 <talwind1@gmail.com>
2021-11-08 22:06:26 +0200

    4.3 exercise upload

---
+++ js-ex/basketball.js
@@ -0,0 +1,13 @@
+let mikeAvg = (116 + 94 + 123) / 3;
+let johnAvg = (89 + 120 + 103) / 3;
+
+let winner;
+if (mikeAvg > johnAvg) {
+  winner = mikeAvg;
+  console.log(`Mike's team win. Their average score is ${winner}`);
+} else if (mikeAvg < johnAvg) {
+  winner = johnAvg;
+  console.log(`John's team win. Their average score is ${winner}`);
+} else {
+  console.log("Both teams win");
+}
