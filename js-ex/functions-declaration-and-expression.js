2afe14d1fe9589b05e4af3c53f95e53e61d62b95
Talwind1 <talwind1@gmail.com>
2021-11-08 14:00:38 +0200

    third exercise

---
+++ js-ex/functions-declaration-and-expression.js
@@ -0,0 +1,13 @@
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+let isrealPercentage = percentageOfWorld1(9);
+let usaPercentage = percentageOfWorld1(330);
+let cyprusPercentage = percentageOfWorld1(1);
+
+console.log(isrealPercentage);
+
+console.log(usaPercentage);
+
+console.log(cyprusPercentage);
