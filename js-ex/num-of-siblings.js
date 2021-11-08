f8518ea361e6e1464bfe0296b09c94de8616faa9
Talwind1 <talwind1@gmail.com>
2021-11-08 21:30:44 +0200

    4th ex

---
+++ js-ex/num-of-siblings.js
@@ -0,0 +1,13 @@
+let numSiblings;
+
+function sibling() {
+  numSiblings = window.prompt("How many siblings do you have?");
+  if (numSiblings == 1) {
+    console.log("1 sibling!");
+  } else if (numSiblings > 1) {
+    console.log("More than 1 sibling");
+  } else {
+    console.log("No siblings");
+  }
+}
+sibling();
