afe515f80e8a1765df5a10cda32b2256c52c5698
Talwind1 <talwind1@gmail.com>
2021-11-09 12:24:35 +0200

    5.4 exercise

---
+++ dark-or-light-color.js
@@ -0,0 +1,15 @@
+function darkLightColor(color) {
+  color = color.toLowerCase();
+  switch (color) {
+    case "yellow":
+    case "pink":
+    case "orange":
+      return "light color";
+    case "blue":
+    case "purple":
+    case "brown":
+      return "dark color";
+    default:
+      return "unknown color";
+  }
+}
