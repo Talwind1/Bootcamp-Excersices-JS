d3126406a899863a72ff93e11a12406752ab52ba
Talwind1 <talwind1@gmail.com>
2021-11-10 23:29:38 +0200

    8.4 update

--- exercises/8.4-count_the_letters.js
+++ exercises/8.4-count_the_letters.js
@@ -1,17 +1,17 @@
 const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
 
 function letterCount(array) {
-  const dict = {};
+  const freq = {};
   for (let i = 0; i < array.length; i++) {
     let word = array[i].toLowerCase().replace(" ", "");
     for (let j = 0; j < word.length; j++) {
-      if (dict[word[j]]) {
-        dict[word[j]] += 1;
+      if (freq[word[j]]) {
+        freq[word[j]] += 1;
       } else {
-        dict[word[j]] = 1;
+        freq[word[j]] = 1;
       }
     }
   }
-  return dict;
+  return freq;
 }
 console.log(letterCount(array));
