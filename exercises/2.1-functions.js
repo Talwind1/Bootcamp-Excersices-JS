cdc343f6d654f9f872ab49a99821ae3aa4e12fa4
Talwind1 <talwind1@gmail.com>
2021-11-11 10:31:35 +0200

    ex 2 update

--- exercises/2.1-functions.js
+++ exercises/2.1-functions.js
@@ -1,5 +1,3 @@
 function tellFortune(jobTitle, location, partnerName, numberOfChildren) {
   return `You will be a ${jobTitle} in ${location} and married to ${partnerName} with ${numberOfChildren} children.`;
 }
-
-console.log(tellFortune("ta", "TLV", "Tal", 4));
