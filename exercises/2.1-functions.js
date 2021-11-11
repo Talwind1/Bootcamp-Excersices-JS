eae20f5d730e1feba4410db4bb12a662756828ba
Talwind1 <talwind1@gmail.com>
2021-11-11 10:30:57 +0200

    ex 2 update

--- exercises/2.1-functions.js
+++ exercises/2.1-functions.js
@@ -1,4 +1,5 @@
 function tellFortune(jobTitle, location, partnerName, numberOfChildren) {
-  return `You will be a ${jobTitle} in ${location} 
-    and married to ${partnerName} with ${numberOfChildren} children.`;
+  return `You will be a ${jobTitle} in ${location} and married to ${partnerName} with ${numberOfChildren} children.`;
 }
+
+console.log(tellFortune("ta", "TLV", "Tal", 4));
