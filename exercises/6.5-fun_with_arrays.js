c1cc0fa0a115556e1d2ad36bc6c9b58cad78c47e
Talwind1 <talwind1@gmail.com>
2021-11-12 15:41:36 +0200

    6.5 eex

--- exercises/6.5-fun_with_arrays.js
+++ exercises/6.5-fun_with_arrays.js
@@ -1,20 +1,22 @@
-let sisters = new Array(100);
+let sister = new Array(100);
 let lior = {};
-sisters.fill(lior);
+sister.fill(lior);
+
+let arr100 = Array.from({ length: 100 }, (v, k) => k + 1); //arr fill with 1-100
 
 let person = {
   name: "Tal",
   age: 28,
   city: "Tlv",
 };
-let values = Object.values(person);
 
-Array.isArray(values);
+let values = Object.values(person); //convert Obj values to array
 
-const arr = (values) => {
-  for (let item of values) {
-    arr[item] = values[item];
-  }
-  return arr;
-};
-console.log(arr);
+let newObj = Object.assign({}, values); //convert array to an Object
+console.log(newObj);
+
+Array.isArray(values); // check if it's Array
+
+let sisters = ["Carmit", "Tal", "Lior"];
+const nameArrCopy = Array.from(sisters); // copy from another Array! a different prototype
+let copySisters = sisters; // creating a copy the influences the original
