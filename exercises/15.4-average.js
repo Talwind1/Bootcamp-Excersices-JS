55e7a724290d786f7919b00aef89cc86a6257107
Talwind1 <talwind1@gmail.com>
2021-11-20 19:37:31 +0200

    15.4 ex

--- exercises/15.4-average.js
+++ exercises/15.4-average.js
@@ -6,3 +6,19 @@ function calcAverage(arr) {
   return sum;
 }
 calcAverage([85, 90, 92]);
+
+//at line 2 the sum is not initialize and it equal to NaN.
+
+//I used the browser debugger under scope section,
+//and I saw that scope section that sun is equal to NaN
+//and what we add numbers to NaN we get Nan :(
+// my fix:
+
+function calcAverage(arr) {
+  let sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+calcAverage([85, 90, 92]);
